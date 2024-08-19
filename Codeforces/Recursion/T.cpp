a3086c14f2403b07df1b80206a308baebd8bdd93
Student-IIUC <mi2670952@gmail.com>
2024-08-19 23:33:10 +0600

    Update Files

--- Codeforces/Recursion/T.cpp
+++ Codeforces/Recursion/T.cpp
@@ -2,18 +2,26 @@
 
 using namespace std;
 
-long long nCr(long long n)
+// Recursive function to compute NCR
+long long nCr(int N, int R)
 {
-    if (n < 2)
-        return 1;
-    return n * nCr(n - 1);
+    if (R == 0 || R == N)
+        return 1; // Base cases: NCR = 1 when R is 0 or R is N
+
+    // Recursive relation: NCR = NCR(N-1, R-1) + NCR(N-1, R)
+    return nCr(N - 1, R - 1) + nCr(N - 1, R);
 }
 
 int main()
 {
-    long long n, r;
-    cin >> n >> r;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    long long b = nCr(r) * nCr(n - r);
-    cout << nCr(n) / b;
+    int N, R;
+    cin >> N >> R;
+    if (N < R)
+        cout << 0 << endl;
+    else
+        cout << nCr(N, R) << endl;
 }
